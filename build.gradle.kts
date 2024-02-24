215cf9d4ddde358910387b88eec167fd3414f940
JDan <110744245+soyJDan@users.noreply.github.com>
2024-02-25 00:13:14 +0100

    Delete publishing Sonatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,13 +37,5 @@ publishing {
                 password = System.getenv("GITHUB_TOKEN")
             }
         }
-        maven {
-            name = "MavenCentral"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = System.getenv("OSSRH_USERNAME")
-                password = System.getenv("OSSRH_TOKEN")
-            }
-        }
     }
 }
